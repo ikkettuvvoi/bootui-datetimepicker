8e919e92e49d8405d37f3bce4cca03b12f5be7fa
ikkettuvvoi <jad.zaza@gmail.com>
2017-11-16 15:14:13 +0100

    Update Datepicker.php

--- Datepicker.php
+++ Datepicker.php
@@ -33,6 +33,7 @@ class Datepicker extends BasePicker
 {	
 	public function init()
 	{
+		$this->pick['date'] = true;
 		parent::init();
 		if (!isset($this->clientOptions['format']))
 			$this->clientOptions['format'] = "YYYY-MM-DD";
