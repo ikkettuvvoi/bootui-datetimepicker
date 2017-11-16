c5785fd744d75d18789fd1fb272b26a7d49bc0e2
ikkettuvvoi <jad.zaza@gmail.com>
2017-11-16 15:14:32 +0100

    Update Timepicker.php

--- Timepicker.php
+++ Timepicker.php
@@ -34,6 +34,7 @@ class Timepicker extends BasePicker
 {
 	public function init()
 	{
+		$this->pick['time'] = true;
 		parent::init();
 		if (!isset($this->clientOptions['format']))
 			$this->clientOptions['format'] = "HH:mm";
